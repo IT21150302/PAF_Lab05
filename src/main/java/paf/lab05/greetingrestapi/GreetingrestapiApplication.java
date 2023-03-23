b50c6160159c56c596cc038624277d9fee9a95b0
IT21150302 <it21150302@my.sliit.lk>
2023-03-23 23:39:26 +0530

    Initial Commit

---
+++ src/main/java/paf/lab05/greetingrestapi/GreetingrestapiApplication.java
@@ -0,0 +1,13 @@
+package paf.lab05.greetingrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingrestapiApplication.class, args);
+	}
+
+}
